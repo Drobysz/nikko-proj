09fd1705001e5f497ad75d023d4c4c27ba96deec
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-11 14:52:20 +0100

    * Change path to the according one to vercel hosting * Rename file to translate.py to vercel be able to recognize api path

---
+++ api/translate.py
@@ -0,0 +1,37 @@
+import os
+import deepl
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+# from dotenv import load_dotenv
+from typing import List
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Req(BaseModel):
+    targetLang: str
+    texts: List[str]
+
+@app.post("/")
+def translate(req: Req):
+    key = os.getenv("DEEPL_API_KEY")
+    if not key:
+        raise HTTPException(500, "Missing DEEPL_API_KEY")
+
+    translator = deepl.Translator(key)
+
+    result = translator.translate_text(
+        req.texts,
+        target_lang=req.targetLang.upper()
+    )
+
+    if isinstance(result, list):
+        return {"translations": [r.text for r in result]}
+    return {"translations": [result.text]}
