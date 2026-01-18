b3935ececc77ce80abcd6afc63492b2d4a6b04ba
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-19 00:46:43 +0100

    * Test 19

--- src/pages/home/components/About/About.js
+++ src/pages/home/components/About/About.js
@@ -4,8 +4,8 @@ export function About() {
 	const nikkoTitle = "Sacred Nikko";
 	const nikkoText = `The Nikko Shrines and Temples complex is a unique cultural landscape where Shinto and Buddhist traditions have coexisted for centuries. Set in the forested mountains of Tochigi Prefecture, the site reflects the close connection between architecture, nature, and ritual. Richly decorated sanctuaries and more restrained sacred spaces together express the spiritual, historical, and artistic legacy of Nikko.`;
 
-	textLib.addText(nikkoTitle, "team_title");
-	textLib.addText(nikkoText, "addressing_visitors");
+	textLib.addText(nikkoTitle, "nikko_title");
+	textLib.addText(nikkoText, "nikko_desc");
 
 	return /*html*/`
 	<section class="nikko_section">
